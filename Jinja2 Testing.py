02b11204a59f1449afc3b8650c2497853d56b530
cyber-lacoco <sarafasatar@gmail.com>
2024-05-08 09:52:30 +0100

    Changes

--- Jinja2 Testing.py
+++ Jinja2 Testing.py
@@ -5,10 +5,14 @@ app = Flask(__name__)
 bootstrap = Bootstrap(app)
 #working with Jinja2 Engine
 #mera 
-
+name = "Satar"
 @app.route('/')
 def home():
-    return render_template('index.html', name="Satar")
+    return render_template('index.html', Name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+ return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
